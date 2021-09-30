d6142715e6e45317e1b41cbcfa438d47a82f9cdf
Ori melamed <orimelamed@Oris-Air.lan>
2021-09-30 15:30:08 +0300

    add validates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,6 @@
 class Invoice < ApplicationRecord
-    
+    validets :issue_date, presence: true
+    validates :maturity_date, presence: true
+    validates :amount, presence: true
+    validates :vendor_name, presence: true
 end
