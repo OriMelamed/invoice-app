b34d8575132af14ddf50424802cd5452fd22829a
Ori melamed <orimelamed@Oris-Air.lan>
2021-09-30 21:49:00 +0300

    add new invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,18 @@
 class InvoicesController < ApplicationController
     def show
+        @invoice = Invoice.find(params[:id])
     end
 
     def index
-        @invoice = Invoice.all
+        @invoices = Invoice.all
+    end
+
+    def new
+    end
+
+    def create
+        @invoice  = Invoice.new(params.require(:invoice).permit(:issue_date, :maturity_date, :amount, :vendor_name))
+        @invoice.save
+        redirect_to @invoice
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :invoices, only: [:show, :index]
+  resources :invoices, only: [:show, :index, :new, :create]
 end
